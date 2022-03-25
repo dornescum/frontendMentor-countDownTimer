56adda6d4252cf41d1f851c35498c85966e976a8
Mihai Dornescu <dornescu@gmail.com>
2022-03-25 13:07:42 +0200

    sm & lg

--- public/index.js
+++ public/index.js
@@ -7,35 +7,28 @@ let countDownDate;
 let todayDate;
 let interval;
 let distanceTime;
-let totalSeconds;
 
 
 function setTime() {
-	countDownDate = new Date("Mar 25, 2024, 12:29:22").getTime();
+	countDownDate = new Date("Mar 25, 2023, 12:47:22").getTime();
 	todayDate = new Date().getTime();
-	 distanceTime = countDownDate - todayDate;
+	distanceTime = countDownDate - todayDate;
 	daysEl.innerHTML = Math.floor(distanceTime / (1000 * 60 * 60 * 24));
 	hoursEl.innerHTML = Math.floor(distanceTime % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
 	minutesEl.innerHTML = Math.floor(distanceTime % (1000 * 60 * 60) / (1000 * 60));
 	secondsEl.innerHTML = Math.floor(distanceTime % (1000 * 60) / 1000);
+
+
 }
 
 function startTimer() {
 	interval = setInterval(() => {
 		setTime();
-		//
 		// if (distanceTime === 0) {
 		// 	stopTimer()
 		// }
 	}, 1000);
 }
-// function stopTimer() {
-// 	// interval = clearInterval(interval);
-// 	// countDownDate = new Date("Mar 25, 2024, 12:22:22").getTime();
-// 	// daysEl.innerHTML =0;
-// 	// hoursEl.innerHTML = 0;
-// 	// minutesEl.innerHTML = 0;
-// 	// secondsEl.innerHTML = 0;
-// }
+
 
 document.addEventListener("DOMContentLoaded", startTimer);
